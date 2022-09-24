3a1c688b906600dedf9cedce3a2e500fe4868873
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-24 17:45:07 +0300

    add user and location reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
 
+import locationReducer from './slicer/locationSlice';
 import themeReducer from './slicer/themeSlice';
+import userReducer from './slicer/userSlice';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
+    user: userReducer,
+    location: locationReducer,
   },
 });
